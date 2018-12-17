cad00fa437f883cef9327dff8a8d19c36b1e7abe
user-PC\user <ppttbb9461@naver.com>
2018-12-18 01:22:13 +0900

    날짜 시간 입력구현

--- classroom finder/app/src/main/java/kr/co/company/ClassroomFinder/sub2.java
+++ classroom finder/app/src/main/java/kr/co/company/ClassroomFinder/sub2.java
@@ -1,14 +1,114 @@
 package kr.co.company.ClassroomFinder;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import java.util.Calendar;
 
-public class sub2 extends AppCompatActivity {
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class sub2 extends Activity {
+
+    //View
+    private TextView mText;
+    private Button mPickDate;
+    private Button mPickTime;
+
+    //년,월,일,시,분
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    private int mHour;
+    private int mMinute;
+
+    //Dialog
+    static final int DATE_DIALOG_ID = 0;
+    static final int TIME_DIALOG_ID = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sub);
+
+        //View 참조
+        mText = (TextView)findViewById(R.id.text);
+        mPickDate = (Button)findViewById(R.id.pickDate);
+        mPickTime = (Button)findViewById(R.id.pickTime);
+
+        //날짜설정 이벤트
+        mPickDate.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+
+        //시간설정 이벤트
+        mPickTime.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(TIME_DIALOG_ID);
+            }
+        });
+
+        //현재 날짜,시간 가져오기
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        //텍스트뷰 갱신
+        updateDisplay();
+    }
+
+    //텍스트뷰 갱신
+    private void updateDisplay(){
+        mText.setText(String.format("%d년 %d월 %d일 %d시 %d분", mYear, mMonth+1, mDay, mHour, mMinute));
+    }
+
+    //DatePicker 리스너
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    mYear = year;
+                    mMonth = monthOfYear;
+                    mDay = dayOfMonth;
+                    updateDisplay();
+                }
+            };
+
+    //TimePicker 리스너
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener(){
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    mHour = hourOfDay;
+                    mMinute = minute;
+                    updateDisplay();
+                }
+            };
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this, mDateSetListener, mYear, mMonth, mDay);
+
+            case TIME_DIALOG_ID :
+                return new TimePickerDialog(this, mTimeSetListener, mHour, mMinute, false);
+        }
+
+        return null;
     }
 
 
